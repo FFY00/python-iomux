025eba648ca63a4733dbcc4d3cb73c3fbbf37e07
Filipe Laíns <lains@riseup.net>
2021-06-01 17:59:31 +0100

    iomux: make T and V typevars private
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- iomux.py
+++ iomux.py
@@ -10,19 +10,19 @@ if typing.TYPE_CHECKING:  # pragma: no cover
     from typing import Any, Iterator, List, Optional, Tuple, Type, Union
 
 
-T = typing.TypeVar('T', bound=io.IOBase)
-# same as T but we need it becuase we use it in a different class
-V = typing.TypeVar('V', bound=io.IOBase)
+_T = typing.TypeVar('_T', bound=io.IOBase)
+# same as _T but we need it becuase we use it in a different class
+_V = typing.TypeVar('_V', bound=io.IOBase)
 
 
-class IOMux(typing.Generic[T]):
-    class Proxy(typing.Generic[V]):
-        def __init__(self, owner: IOMux[V], name: str) -> None:
+class IOMux(typing.Generic[_T]):
+    class Proxy(typing.Generic[_V]):
+        def __init__(self, owner: IOMux[_V], name: str) -> None:
             self._owner = owner
             self._name = name
 
         @property
-        def _io(self) -> V:
+        def _io(self) -> _V:
             if not self._owner._io or self._owner._io[-1][0] != self._name:
                 self._owner._io.append((self._name, self._owner._io_cls()))
             return self._owner._io[-1][1]
@@ -31,19 +31,19 @@ class IOMux(typing.Generic[T]):
             return getattr(self._io, name)
 
     def __init__(self) -> None:
-        self._io_cls: Type[T] = self.__orig_bases__[0].__args__[0]
-        self._io: List[Tuple[str, T]] = []
+        self._io_cls: Type[_T] = self.__orig_bases__[0].__args__[0]
+        self._io: List[Tuple[str, _T]] = []
 
     def __getattr__(self, name: str) -> Any:
         return self.Proxy(self, name)
 
     @typing.overload
-    def entries(self) -> Iterator[Tuple[str, T]]: ...
+    def entries(self) -> Iterator[Tuple[str, _T]]: ...
 
     @typing.overload
-    def entries(self, name: str) -> Iterator[T]: ...
+    def entries(self, name: str) -> Iterator[_T]: ...
 
-    def entries(self, name: Optional[str] = None) -> Union[Iterator[Tuple[str, T]], Iterator[T]]:
+    def entries(self, name: Optional[str] = None) -> Union[Iterator[Tuple[str, _T]], Iterator[_T]]:
         if name is None:
             yield from self._io
         for io_name, io_obj in self._io:
